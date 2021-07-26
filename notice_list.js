05205fe930725c9c065a70a056d5c1a0f6488dd1
WONDAEHAN <74443021+wondaehan@users.noreply.github.com>
2021-07-26 10:36:41 +0900

    Add files via upload

---
+++ notice_list.js
@@ -0,0 +1,4 @@
+var titles = ['makebot projects documents page opened', 'makebot mail information', 'project page has been updated', 'project pages and makebot robots app page imformation', 'site stabilization period information', 'makebot project copyright notice', 'The domain of the makebot site will be transferred'];
+var ids = ['106', '105', '104', '103', '102', '101', '100'];
+var dates = ['7.24.2021', '6.30.2021', '6.29.2021', '6.10.2021', '5.22.2021', '5.5.2021', '4.26.2021'];
+var writers = ['makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot'];
