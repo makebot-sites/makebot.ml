9b626985e5a33e60c8d1563240bd5e66e47c7129
WONDAEHAN <74443021+wondaehan@users.noreply.github.com>
2021-10-08 21:19:45 +0900

    Delete ko directory

--- ko/notice_list.js
+++
@@ -1,4 +0,0 @@
-var titles = ['makebot 프로젝트 공식문서 페이지 오픈', 'makebot 메일 관련 안내', '프로젝트 페이지 업데이트 사항', '프로젝트 페이지와 makebot robots app 페이지 관련 안내', '사이트 안정화 기간 안내', 'makebot 프로젝트 저작권', '도메인 이전 안내'];
-var ids = ['106', '105', '104', '103', '102', '101', '100'];
-var dates = ['2021.7.24', '2021.6.30', '2021.6.29', '2021.6.10', '2021.5.22', '2021.5.5', '2021.4.26'];
-var writers = ['makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot'];
