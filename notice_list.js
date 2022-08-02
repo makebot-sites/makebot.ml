3a4ee6e718a5bd4e6b86ee0dc890d3a2d7496481
Daehan Won <100026368+DaehanWon@users.noreply.github.com>
2022-08-02 11:52:40 +0900

    Update notice_list.js

--- notice_list.js
+++ notice_list.js
@@ -1,4 +1,4 @@
-var titles = ['R BOT, pengbot, and ottopus libraries released', 'beepbot python package released', 'makebot projects documents page opened', 'makebot mail information', 'project page has been updated', 'project pages and makebot robots app page imformation', 'site stabilization period information', 'makebot project copyright notice', 'The domain of the makebot site will be transferred'];
-var ids = ['108', '107', '106', '105', '104', '103', '102', '101', '100'];
-var dates = ['1.10.2022', '12.25.2021', '7.24.2021', '6.30.2021', '6.29.2021', '6.10.2021', '5.22.2021', '5.5.2021', '4.26.2021'];
-var writers = ['makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot'];
+var titles = ['R BOT, pengbot, and ottopus libraries released', 'makebot projects documents page opened', 'makebot mail information', 'project page has been updated', 'project pages and makebot robots app page imformation', 'site stabilization period information', 'makebot project copyright notice', 'The domain of the makebot site will be transferred'];
+var ids = ['107', '106', '105', '104', '103', '102', '101', '100'];
+var dates = ['1.10.2022', '7.24.2021', '6.30.2021', '6.29.2021', '6.10.2021', '5.22.2021', '5.5.2021', '4.26.2021'];
+var writers = ['makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot'];
